032797d0c53613cec97c1846a4274d3f70070744
txtxj <1418373194@qq.com>
2022-03-27 23:49:40 +0800

    feat: finish Render

--- Assignment5/Renderer.cpp
+++ Assignment5/Renderer.cpp
@@ -223,8 +223,8 @@ void Renderer::Render(const Scene& scene)
         for (int i = 0; i < scene.width; ++i)
         {
             // generate primary ray direction
-            float x;
-            float y;
+            float x = (((float)i + 0.5f) / (float)scene.width * 2.0f - 1.0f) * imageAspectRatio;
+            float y = 1.0f - ((float)j + 0.5f) / (float)scene.width * 2.0f;
             // TODO: Find the x and y positions of the current pixel to get the direction
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
